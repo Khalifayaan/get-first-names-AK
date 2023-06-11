528bdace9257829cd85ad5fdf57d6617c87126ad
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-11 07:04:07 +0300

    Completed question 2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Good Luck 💪🏾
+
+
+function getFirstNames(names) {
+    let firstNames = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let firstName = names[i][0];
+      firstNames[i] = firstName;
+    }
+  
+    return firstNames;
+  }
+
+  console.log(getFirstNames([["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]]));
